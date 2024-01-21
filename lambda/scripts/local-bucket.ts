26430065c749df0ad00480373ea8989352357f77
jfree <jfreeman.oh@gmail.com>
2024-01-21 12:09:52 -0500

    commit

---
+++ lambda/scripts/local-bucket.ts
@@ -0,0 +1,29 @@
+import { S3Client, CreateBucketCommand, ListBucketsCommand } from '@aws-sdk/client-s3'
+
+let s3Client: s3Client
+
+const getClient = (region: string, endpoint: string) => {
+    if (!s3Client) {
+        s3Client = new s3Client({
+            region,
+            endpoint,
+            forcePathStyle: true
+        })
+    }
+    return s3Client
+}
+
+export const localBucket = {
+    async create () {
+        const region = 'us-east-1'
+        const endpoint = 'http://localhost:4566'
+        const s3 = getClient(region, endpoint)
+        const command = new CreateBucketCommand({
+            Bucket: 'local-bucket'
+        })
+        await s3.send(command)
+        const buckets = await s3.send(new ListBucketsCommand({})
+        console.log('buckets', buckets)
+        process.env.BUCKET_NAME = 'local-buckets'
+    }
+}
